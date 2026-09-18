fc2dc1b50d07b7607a3fc4d90d29c4d4642a0321
Saurbh 026 <saurabh239goswami@gmail.com>
2026-09-19 02:32:03 +0530

    fix: allow all CORS origins for production

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const authRouter   = require('./routes/auth');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors());
 app.use(express.json());
 
 mongoose
